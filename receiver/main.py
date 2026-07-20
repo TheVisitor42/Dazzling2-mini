59dd650685bba20121b2fef61f7670a6339748f1
John Pointer <heresjohny94@outlook.com>
2026-07-19 22:52:02 -0500

    Update main.py

--- receiver/main.py
+++ receiver/main.py
@@ -84,6 +84,67 @@ def check_sequence(packet):
 
 print("Receiver started...\n")
 
+
+
+
+def display_weather(packet):
+
+    weather = packet["data"]
+
+    print('TEMP:',weather["temperature"])
+    print('WIND SPEED:',weather["wind_speed"])
+    print('Sunrise:',weather["sunrise"])
+    print('Sunset:',weather["sunset"])
+    print('Percipitation:',weather["percipitation"])
+
+#customize displays here.
+
+def display_stocks(packet):
+
+    stocks = packet["data"]
+    print("Packet received")
+        print("Type:", packet["type"])
+        print("Mode:", packet["mode"])
+    print(stocks["BRK.B"]["price"])
+
+def display_news(packet):
+    news = packet["data"]
+
+    print(news["top_stories"])
+
+elif display_clock(packet):
+
+    clock = packet["data"]
+
+    print(clock["time"])
+else:
+
+    unkown = packet["data"]
+
+    print(unkown["debug"])
+    
+#Process packet
+def process_packet(packet):
+
+    mode = packet["mode"]
+
+    if mode == "weather":
+        display_weather(packet)
+
+    elif mode == "stocks":
+        display_stocks(packet)
+
+    elif mode == "news":
+        display_news(packet)
+
+    elif mode == "clock":
+        display_clock(packet)
+
+    else:
+        print("Unknown mode:", mode)
+
+'''    
+#old while true loop
 while True:
 
     packet = receive_packet()
@@ -106,59 +167,23 @@ while True:
             pass
 
     time.sleep(0.01)
+'''
 
-def process_packet(packet):
+#new while true loop
 
-    mode = packet["mode"]
+while True:
 
-    if mode == "weather":
-        display_weather(packet)
+    packet = receive_packet()
 
-    elif mode == "stocks":
-        display_stocks(packet)
+    if packet is not None:
 
-    elif mode == "news":
-        display_news(packet)
+        missing = check_sequence(packet)
 
-    elif mode == "clock":
-        display_clock(packet)
+        if not missing:
 
-    else:
-        print("Unknown mode:", mode)
+            process_packet(packet)
 
-def display_weather(packet):
-
-    weather = packet["data"]
-
-    print('TEMP:',weather["temperature"])
-    print('WIND SPEED:',weather["wind_speed"])
-    print('Sunrise:',weather["sunrise"])
-    print('Sunset:',weather["sunset"])
-    print('Percipitation:',weather["percipitation"])
+    time.sleep(0.01)
 
 
-def display_stocks(packet):
-
-    stocks = packet["data"]
-
-    print(stocks["BRK.B"]["price"])
-
-def display_news(packet):
-    news = packet["data"]
-
-    print(news["top_stories"])
-
-elif display_clock(packet):
-
-    clock = packet["data"]
-
-    print(clock["time"])
-else:
-
-    unkown = packet["data"]
-
-    print(unkown["debug"])
-
-
-    
     
