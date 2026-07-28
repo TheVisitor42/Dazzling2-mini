569de113713feadf136396adc0c7c5d7fe8fd27f
John Pointer <heresjohny94@outlook.com>
2026-07-27 19:36:59 -0500

    Update multiplexer.py

--- receiver/multiplexer.py
+++ receiver/multiplexer.py
@@ -1,1 +1,32 @@
-#This code will be used heavily in conjunction with the oled.py script. The multiplexer drives the identically addressed oled's.
+from machine import I2C, Pin
+import time
+
+
+i2c = I2C(
+    1,
+    scl=Pin(3),
+    sda=Pin(2),
+    freq=400000
+)
+
+
+TCA_ADDR = 0x70
+
+
+def select_mux_channel(channel):
+
+    if channel > 3:
+
+        i2c.writeto(
+            TCA_ADDR,
+            bytes([0])
+        )
+
+    else:
+
+        i2c.writeto(
+            TCA_ADDR,
+            bytes([1 << channel])
+        )
+
+    time.sleep_ms(1)
